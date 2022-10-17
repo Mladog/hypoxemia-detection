e1dc226b2a79f0a7e4e4db3c399af1c867d30dd1
Magda <m.lado@wp.pl>
2022-10-17 21:40:37 +0200

    add BreathSignal template

---
+++ breath_signal.py
@@ -0,0 +1,24 @@
+
+
+class BreathSignal():
+
+    def __init__(self, fs= 500):
+        self.freq = fs
+
+    def fs_inst():
+        pass
+
+    def TV_insp():
+        pass
+
+    def TV_exp():
+        pass
+
+    def DUR_insp():
+        pass
+
+    def DUR_exp():
+        pass
+
+    def trend():
+        pass
---
+++ main.py
@@ -0,0 +1,2 @@
+# IMPORT REQUIRED LIBRARIES
+from breath_signal import BreathSignals
